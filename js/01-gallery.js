ac7a3cfb08c28ebcac9717fba075555b1add4feb
vitasika <vitasika1@gmail.com>
2022-01-25 23:39:07 +0200

    начал что то по 7 дз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// Найти строку в html с названием class="gallery"
+const galleryEl = document.querySelector('.gallery');
+
+console.log('работает');
+console.log(galleryEl);
 console.log(galleryItems);
+
+
+/*  шаблон элемента галереи
+
+<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>
+
+*/
