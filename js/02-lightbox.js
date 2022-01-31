078a12b24e7c5ebca93fe40c7fb01ebfa31cc46a
vitasika <vitasika1@gmail.com>
2022-01-31 23:13:27 +0200

    начал делать 7-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// Найти строку в html с названием class="gallery"
+  const galleryEl = document.querySelector(".gallery");  
+
+
+  //Преобразовал galleryItems в одну строку с параметрами
+const galleryCardEl = galleryItems.map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href=""${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+});
+
+
+  
